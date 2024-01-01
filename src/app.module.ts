3ad5334aa3828c2a09aebe72deef64651c93a687
Marco Gerstmann <mail@marcogerstmann.com>
2024-01-01 10:03:33 +0100

    remove unused app classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { OpenAIModule } from './openai/openai.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), OpenAIModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
