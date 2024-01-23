7418015968eb4fd62f28c3f3ceb5f64f87146da7
Marco Gerstmann <mail@marcogerstmann.com>
2024-01-23 10:48:18 +0100

    add info endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { OpenAIModule } from './openai/openai.module';
 import { SolarModule } from './solar/solar.module';
 import { GeoModule } from './geo/geo.module';
+import { InfoController } from './info/info.controller';
 
 @Module({
   imports: [ConfigModule.forRoot(), OpenAIModule, SolarModule, OpenAIModule, GeoModule],
+  controllers: [InfoController],
 })
 export class AppModule {}
